2a96ad11af603361ffc43d2323ccde96c8d7c8e7
Maghan Das <dasmaghan360@gmail.com>
2025-02-26 21:38:15 +0000

    variables in python

---
+++ Variables & Data Types/variables.py
@@ -0,0 +1,36 @@
+# to run the code..
+# python3 filename
+
+# print("Hello World!");
+
+
+age = 23;
+name = "Raj";
+# print(age);
+Age = str(age); # type casting from int to string
+# print(type(Age)); # type() is used to check the type of the variable
+
+# print("My name is " + name + " and my age is " + Age);
+# print(f"My name is {name} and my age is {Age}");
+# print(name[0]); 
+
+isprogrammer, isdesigner = True, False;
+# print(isprogrammer, isdesigner);
+
+# name = input("Enter your name: ");
+# print(f"Hello {name}");
+
+# age,isPro,country = 23,True,"Hungary";
+# print(age,isPro,country)
+
+# if age > 18:
+#   print("You are 18+");
+# elif age == 18:
+#   print("You are 18");
+# else:
+#   print(f"You are {age} years old");
+
+# name=input()
+# age = int(input("Enter your age: "));
+# print(type(age))
+# print(f"Hello {name}, you are {age} years old")
