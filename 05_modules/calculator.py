cfec8543cfddd899ec861ab0a27393ccc4d08ab3
Maghan Das <dasmaghan360@gmail.com>
2025-03-03 20:34:08 +0000

    modules in python

---
+++ 05_modules/calculator.py
@@ -0,0 +1,18 @@
+
+#  Create a module called calculator.py with functions to add, subtract, multiply, and divide two numbers.
+# # In the main program, import the calculator module and test its functions
+
+def add(x,y):
+    return x+y
+
+
+def sub(x,y):
+    return x-y
+
+
+def mul(x,y):
+    return x*y
+
+
+def div(x,y):
+    return x//y
