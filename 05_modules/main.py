4b55f3068be637ef4e056fbdb830062115a73b40
Maghan Das <dasmaghan360@gmail.com>
2025-03-03 20:35:20 +0000

    modules in python

---
+++ 05_modules/main.py
@@ -0,0 +1,14 @@
+
+# import mymodule
+import calculator
+
+# name = input("Enter your name: ")
+# print(mymodule.greet(name))
+
+num1 = int(input("enter first num: "))
+num2 = int(input("enter first num: "))
+
+print(calculator.add(num1,num2))
+print(calculator.sub(num1,num2))
+print(calculator.mul(num1,num2))
+print(calculator.div(num1,num2)) 
