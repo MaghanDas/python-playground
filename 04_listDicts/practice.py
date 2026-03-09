34478f2b8e423225a4aa56faf9495348b921bce9
Maghan Das <dasmaghan23@gmail.com>
2026-03-09 17:20:58 +0000

    hashmap

--- 04_listDicts/practice.py
+++ 04_listDicts/practice.py
@@ -9,6 +9,9 @@
 
 # practicing dictionaries and lists
 
+from unicodedata import name
+
+
 number = [1, 2, 3, 4, 5]
 squares = {x: x*x for x in number}
 print(squares)
@@ -18,3 +21,29 @@ text = "apple banana apple orange banana apple"
 word_count = {word: text.count(word) for word in text.split()}
 print(word_count)
 
+
+# dictionary (hashmaps) is data structure that allows
+# very fast lookups, insertions, and deletions based on keys. 
+# ex: person = {
+#     "name": "Das",
+#     "age": 23,
+#     "city": "Budapest"
+# }
+# intenernally python: 
+# index | key    | value
+# -----------------------
+# 3     | name   | Das
+# 7     | age    | 23
+# 1     | city   | Budapest
+
+# that index comes from hash function that takes the key and returns a unique index for it.
+# hash function is a function that takes an input (or "key") and returns a fixed-size string of bytes.
+# hash (name) # output then python converts into an index isnide th hash table 
+
+# when we write person["name"] = "das"
+# python calculate of "name" and converts to index: 
+# and then store key-value pair at that index in the hash table.
+
+# hash collisition when two keys produces the same index.
+# keys must be immutable so their hash never chnages
+# list as key can't be allowed because they are mutable and their hash can change, which would cause issues in the hash table.
