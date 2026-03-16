7e7e967e7cf2a02d82a0039f6192df96ef9a7805
Maghan Das <dasmaghan23@gmail.com>
2026-03-16 23:19:53 +0000

    file handling file structure modified

--- 07_file_handling/exceptionHandling.py
+++ 07_file_handling/exceptionHandling.py
@@ -11,12 +11,14 @@
 # except ValueError:
 #     print("Nope! that's not the correct num.")
 
-try:
-    num1 = int(input("enter first number: "))
-    num2 = int(input("enter first number: "))
-    print("Division is ", num1 // num2)
-except ValueError:
-       print("Nope! that's not the correct num.")
-except ZeroDivisionError:
-      print("Eror! can't divide by zero")
+# try:
+#     num1 = int(input("enter first number: "))
+#     num2 = int(input("enter first number: "))
+#     print("Division is ", num1 // num2)
+# except ValueError:
+#        print("Nope! that's not the correct num.")
+# except ZeroDivisionError:
+#       print("Eror! can't divide by zero")
     
+
+
