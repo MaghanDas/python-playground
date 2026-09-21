d7f90369c3e251d7a6c8ade2a498d321aca63d20
Maghan Das <dasmaghan23@gmail.com>
2026-09-21 20:38:48 +0000

    change in loops.py

--- 02_loops&Conditionals/loops.py
+++ 02_loops&Conditionals/loops.py
@@ -1,38 +1,39 @@
 
 # for loop
-# for i in range(5):
-#     print("iteration: " ,i)
+for i in range(5):
+    print("iteration: " ,i)
     
-# for j in range(0,4,2):
-#     print(j)
+# range(start, stop, step)
+for j in range(0,4,2):
+    print(j)
 
 # while looop
-# count = 0
-# while count < 5:
-#   print("count is: ", count)
-#   count += 1
+count = 0
+while count < 5:
+  print("count is: ", count)
+  count += 1
 
 # looping through a list..
-# fruits = ["apple", "banana", "cherry"]
-# for fruit in fruits:
-#   print("I like: ",fruit)
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+  print("I like: ",fruit)
 
-# sum=0
-# for i in range(5,10):
-#   sum+=i
-#   print(i,sum)
-# print(sum)
+sum=0
+for i in range(5,10):
+  sum+=i
+  print(i,sum)
+print(sum)
 
 
-# secret_number = 7
+secret_number = 7
 
-# while True: 
-#     guess = int(input("enter an integer number:"))
-#     if guess==secret_number:
-#         break
-#     print("Haahahah, not correct")
+while True: 
+    guess = int(input("enter an integer number:"))
+    if guess==secret_number:
+        break
+    print("Haahahah, not correct")
 
-# print("GOOD JOB!!!")
+print("GOOD JOB!!!")
 
 
 # ─── LOOPS ─────────────────────────────────────────────
